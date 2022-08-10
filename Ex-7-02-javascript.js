269da5fbfc5136166da6ef8614cafb44bbd64ff2
heminson <sonheungmin79999@gmail.com>
2022-08-10 14:49:22 +0700

    bai tap java

---
+++ Ex-7-02-javascript.js
@@ -0,0 +1,8 @@
+var inputWidth;
+var inputHeight;
+inputWidth = prompt("nhập vào giá trị chiều Rộng:");
+inputHeight = prompt("nhập vào giá trị chiều Cao:");
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+document.write("Kết quả là: " + area);
